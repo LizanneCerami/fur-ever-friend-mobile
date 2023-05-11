0f1f6a5dcd3aa0204cf64bc494d41f63853bbbec
Lizanne Cerami <lizanne.cerami@gmail.com>
2023-05-11 17:20:04 -0400

    added an icon

--- App.js
+++ App.js
@@ -1,7 +1,7 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import Home from "./components/dogPage.jsx";
-import Matches from "./components/matches.jsx";
+import Home from "./components/DogPage.jsx";
+import Matches from "./components/Matches.jsx";
 
 const Stack = createNativeStackNavigator();
 
@@ -9,7 +9,7 @@ export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Home">
-        <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Fur-Ever Friend" component={Home} />
         <Stack.Screen name="Pup Pocket" component={Matches} />
       </Stack.Navigator>
     </NavigationContainer>
